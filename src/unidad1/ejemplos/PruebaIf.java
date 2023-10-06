32e62bc62fd920a4eca6357a69614286db93685b
ciclofp <ciclofp@10.0.2.15>
2023-10-06 13:06:58 +0200

    .

---
+++ src/unidad1/ejemplos/PruebaIf.java
@@ -0,0 +1,20 @@
+package unidad1.ejemplos;
+
+import java.util.Scanner;
+
+public class PruebaIf {
+
+	public static void main(String[] args) {
+	int num;
+	Scanner entrada = new Scanner(System.in);
+	System.out.print("Introduce una edad");
+	num = entrada.nextInt();
+	while (num<18) {
+		System.out.print("Es menor ponga otro:");
+		num = entrada.nextInt();
+	}
+	System.out.println("Eres mayor de edad");
+	
+	
+	}
+}

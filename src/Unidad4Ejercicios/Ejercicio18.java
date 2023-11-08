e174e885239bd23faf60fea693162b2427900393
ciclofp <ciclofp@10.0.2.15>
2023-11-08 10:24:33 +0100

    8/11/23

---
+++ src/Unidad4Ejercicios/Ejercicio18.java
@@ -0,0 +1,28 @@
+package Unidad4Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+		int monedas20Cts;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Cuantás monedas de 20 céntimos tienes?");
+		monedas20Cts=entrada.nextInt();
+		calcularQuintos(monedas20Cts);
+	}
+
+	private static void calcularQuintos(int monedas20Cts) {
+		double euros;
+		euros = monedas20Cts*0.2;
+		imprimir(euros);
+		
+	}
+
+	private static void imprimir(double euros) {
+		System.out.println("Tienes " + euros + " euros en total");
+		
+	}
+
+}
---
+++ src/Unidad4Ejercicios/ProgramaNumero2.java
@@ -0,0 +1,53 @@
+package Unidad4Ejercicios;
+
+import java.util.Scanner;
+
+public class ProgramaNumero2 {
+
+	public static void main(String[] args) {
+		recogerPantalla();
+		
+
+	}
+	
+	private static void recogerPantalla() {
+		int n1;
+		int n2;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Indique el primer número");
+		n1 = entrada.nextInt();
+		System.out.println("Indique el segundo número");
+		n2 = entrada.nextInt();
+		
+		mostrarNumeros(n1, n2);
+		mostrarDobleNumero(n1, n2);
+		mostrar5VecesNumero (n1,n2);
+		mostrarCuadradoNumero(n1,n2);
+	}
+
+	private static void mostrarNumeros(int n1, int n2) {
+		System.out.println("El número 1 es = " + n1 + " y el 2 es = " + n2);
+
+	}
+
+	private static void mostrarDobleNumero(int n1, int n2) {
+		n1 = n1 * 2;
+		n2 = n2 * 2;
+		System.out.println("El doble del primero es = " + n1 + " y el del segundo es = " + n2);
+	}
+	
+	private static void mostrar5VecesNumero(int n1, int n2) {
+		n1 = n1*5;
+		n2 = n2*5;
+		System.out.println("El primero 5 veces es = " + n1 + " y el del segundo es = " + n2);
+	}
+	
+	private static void mostrarCuadradoNumero(int n1, int n2) {
+		n1 = n1*n1;
+		n2 = n2*n2;
+		System.out.println("El cuadrado del  primero veces es = " + n1 + " y el del segundo es = " + n2);
+		
+	}
+
+}

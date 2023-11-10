f2fd70bf0523f8c26f283e147b3ce6a7cf35a300
ciclofp <ciclofp@10.0.2.15>
2023-11-10 10:24:11 +0100

    11/10

---
+++ src/unidad5Ejemplos/EstacionMeteorologica.java
@@ -0,0 +1,29 @@
+package unidad5Ejemplos;
+
+public class EstacionMeteorologica {
+
+	public static void main(String[] args) {
+
+		int[] temperaturas = new int [12];
+		int[] diasLluviosos = { 28, 6, 48, 89 };
+		int[] humedad = { 10, 67, 24, 89, 76, 50 };
+
+		
+		System.out.println("-----Temperaturas medias-----");
+		imprimirArray(temperaturas, "º");
+		System.out.println("-----Diás lluviosos por estaciones-----");
+		imprimirArray(diasLluviosos, " dias");
+		System.out.println("-----Humedad cada 2 meses-----");
+		imprimirArray(humedad, "%");
+		
+	}
+
+
+	private static void imprimirArray(int[] datos, String unidad) {
+
+		for (int i = 0; i < datos.length; i++) {
+			System.out.println("Mes " + (i + 1) + ": " + datos[i] + unidad);
+		}
+	}
+
+}

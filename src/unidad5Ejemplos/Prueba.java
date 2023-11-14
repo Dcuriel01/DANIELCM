a0a5701eb9b398b3923dcb5410add489ebe1b337
ciclofp <ciclofp@10.0.2.15>
2023-11-14 12:44:27 +0100

    Tarea control vendimia dia 14/11/23

---
+++ src/unidad5Ejemplos/Prueba.java
@@ -0,0 +1,25 @@
+package unidad5Ejemplos;
+
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+	
+		
+		int[] array;
+		
+	    Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Dimension:");
+		int dimension = entrada.nextInt();
+		
+		array = new int[dimension];
+		
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		
+	}
+
+}

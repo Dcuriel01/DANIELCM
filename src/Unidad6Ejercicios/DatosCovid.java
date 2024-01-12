b1894d1d6a5a455084d86212cd35085c4714d3c7
ciclofp <ciclofp@10.0.2.15>
2024-01-12 13:20:35 +0100

    12/01/24

---
+++ src/Unidad6Ejercicios/DatosCovid.java
@@ -0,0 +1,11 @@
+package Unidad6Ejercicios;
+
+public class DatosCovid {
+	private String nombre;
+	private int edad;
+	private String localidad;
+	private boolean resultado;
+	private int nPersonasViven;
+	
+	
+}

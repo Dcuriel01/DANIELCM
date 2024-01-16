4dc0c577565acda34c3b38e3566ba328e6dceb92
ciclofp <ciclofp@10.0.2.15>
2024-01-16 11:13:08 +0100

    16/01/24

---
+++ src/Unidad6Ejercicios/Bodega.java
@@ -0,0 +1,83 @@
+package Unidad6Ejercicios;
+
+import java.util.Scanner;
+
+public class Bodega {
+	/*		private String tipoVino;
+		private String nombreVino;
+		private String fechaInicio;
+		private double precioPorLitro;
+		private double cantidadVino;*/
+	
+	public static Scanner sc = new Scanner(System.in);
+	public static String tipoVino;
+	public static String nombreVino;
+	public static String fechaInicio;
+	public static double precioPorLitro;
+	public static double cantidadVino;
+	
+	public static void main(String[] args) {
+		int opcion;
+		int contador=0;
+		int barricaAConsultar;
+		Barrica [] barricas = new Barrica [8];
+		do {
+			opcion=menu();
+			switch (opcion) {
+			case 1:
+				preguntarDatos();
+				barricas[0+contador]=new Barrica(tipoVino,nombreVino,fechaInicio,precioPorLitro,cantidadVino);
+				System.out.println(barricas[0+contador].imprimirDatos());
+				contador++;
+				break;
+			case 2:
+				System.out.println("¿De que barrica desea consultar los datos?");
+				barricaAConsultar=sc.nextInt();
+				barricaAConsultar= barricaAConsultar-1;
+				if (barricas[barricaAConsultar].getTipoVino()==null) {
+					System.out.println("Esa barrica esta vacia aún");
+				}else {
+					System.out.println(barricas[barricaAConsultar].imprimirDatos());
+				}
+				break;
+			case 3:
+				System.out.println("Ok saliendo");
+				break;
+
+			default:
+				System.out.println("No reconocido");
+				break;
+			}
+		} while (opcion!=3);
+		
+		
+		
+
+	}
+
+	private static void preguntarDatos() {
+		System.out.println("¿Qué tipo es el vino almacenado?");
+		tipoVino = sc.next();
+		System.out.println("¿Cuál es el nombre del vino almacenado?");
+		nombreVino = sc.next();
+		System.out.println("¿Cuándo se empezó a fermentar?");
+		fechaInicio=sc.next();
+		System.out.println("¿Cuánto cuesta cada litro del vino?");
+		precioPorLitro=sc.nextDouble();
+		System.out.println("¿Cuántos litros de ese vino hay almacenado?");
+		cantidadVino = sc.nextDouble();
+		
+		
+	}
+
+	private static int menu() {
+		int opcion;
+		System.out.println("-----MENU-----");
+		System.out.println("1.Registrar barrica");
+		System.out.println("2.Consultar datos de barrica");
+		System.out.println("3.Salir");
+		opcion = sc.nextInt();
+		return opcion;
+	}
+
+}
--- src/Unidad6Ejercicios/DatosCovid.java
+++ src/Unidad6Ejercicios/DatosCovid.java
@@ -8,4 +8,45 @@ public class DatosCovid {
 	private int nPersonasViven;
 	
 	
+	
+	public DatosCovid(String nombre, int edad, String localidad, boolean resultado, int nPersonasViven) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.localidad = localidad;
+		this.resultado = resultado;
+		this.nPersonasViven = nPersonasViven;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public boolean isResultado() {
+		return resultado;
+	}
+	public void setResultado(boolean resultado) {
+		this.resultado = resultado;
+	}
+	public int getnPersonasViven() {
+		return nPersonasViven;
+	}
+	public void setnPersonasViven(int nPersonasViven) {
+		this.nPersonasViven = nPersonasViven;
+	}
+	
+	
+	
 }

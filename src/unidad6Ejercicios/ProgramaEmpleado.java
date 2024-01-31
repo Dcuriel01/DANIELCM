e64447ca07c6975d6f525c4c95eda8bd1a0f7c18
diurno <diurno@equipo-00>
2024-01-31 14:22:34 +0100

    Ejercicios empleados

---
+++ src/unidad6Ejercicios/ProgramaEmpleado.java
@@ -0,0 +1,28 @@
+package unidad6Ejercicios;
+
+public class ProgramaEmpleado {
+
+	public static void main(String[] args) {
+		int nEmpleados = 3;
+		Empleado [] empleados = new Empleado[nEmpleados];
+		empleados[0] = new Empleado("Susana Pérez",83738,"Contabilidad","Vicedirectora");
+		empleados[1]= new Empleado("Juan José Ramirez",23119);
+		empleados[2]= new Empleado();
+		
+		empleados[1].setDepartamento("IT");
+		empleados[1].setPuesto("Programador");
+		
+		empleados[2].setNombre("Ana María García");
+		empleados[2].setIdNum(50218);
+		empleados[2].setDepartamento("Fabricación");
+		empleados[2].setPuesto("Ingeniero");
+		imprimirDatos(empleados);
+	}
+	private static void imprimirDatos(Empleado[] empleados) {
+		System.out.println("Nombre    Número ID    Departamento   Posición");
+		for (int i = 0; i < empleados.length; i++) {
+			empleados[i].mostrarDatos();
+		}
+	}
+
+}

33e04e172ad4baa478ac82609ba9f53cb31cb846
diurno <diurno@equipo-00>
2024-02-21 10:18:27 +0100

    Ejercicios cuenta

---
+++ src/unidad8Ejercicios/tarea1/GestorCuenta.java
@@ -0,0 +1,18 @@
+package unidad8Ejercicios.tarea1;
+
+import unidad8Ejercicios.tarea1Part2.CuentaAhorro2;
+import unidad8Ejercicios.tarea1Part2.CuentaCorriente2;
+
+public class GestorCuenta {
+
+	public static void main(String[] args) {
+
+		Cuenta Primeracuenta = new Cuenta("Daniel", 10);
+		
+		System.out.println(Primeracuenta);
+	}
+	
+	
+	
+
+}

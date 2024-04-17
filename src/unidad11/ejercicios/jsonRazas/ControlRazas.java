91f9e9687abfe8f98952596d25b857a1e8082aff
molam <molam@192.168.1.30>
2024-04-17 18:40:19 +0200

    Control de raza acabado

--- src/unidad11/ejercicios/jsonRazas/ControlRazas.java
+++ src/unidad11/ejercicios/jsonRazas/ControlRazas.java
@@ -12,7 +12,8 @@ import com.google.gson.Gson;
 public class ControlRazas {
 
 	public static void main(String[] args) {
-		HashMap<String, ArrayList<Raza>> zona = new HashMap<String, ArrayList<Raza>>();
+		HashMap<String, HashMap <String,Integer>> zona = new HashMap<String, HashMap <String,Integer>>();
+	
 		try {
 			File fichero = new File("ficheros/json/razasComarcasExtremadura.json");
 			BufferedReader lector = new BufferedReader(new FileReader(fichero));
@@ -30,17 +31,25 @@ public class ControlRazas {
 			
 			for (int i = 0; i < razas.length; i++) {
 				if (zona.containsKey(razas[i].getZonas())) {
-					zona.get(razas[i].getZonas()).add(razas[i]);
+					zona.get(razas[i].getZonas()).
+					put(razas[i].getRaza_ganado(), 
+							zona.get(razas[i].getZonas()).getOrDefault(razas[i].getNumero(), 0)+razas[i].getNumero());
 				}else {
-					ArrayList<Raza> lista = new ArrayList<Raza>();
-					lista.add(razas[i]);
-					zona.put(razas[i].getZonas(),zona.getOrDefault(0, lista));
+					HashMap <String,Integer> raza = new HashMap<String,Integer>();
+					raza.put(razas[i].getRaza_ganado(), raza.getOrDefault(razas[i].getNumero(), 0)+razas[i].getNumero());
+					zona.put(razas[i].getZonas(), raza);
 				}
+				
+				
 			}
 			for (String tipo : zona.keySet()) {
 				System.out.println(tipo);
-				
-				
+				for (String tipo2 : zona.get(tipo).keySet()) {
+					System.out.println(tipo2);
+					System.out.println(zona.get(tipo).get(tipo2));
+					System.out.println("************************");
+				}
+				System.out.println("_______________");
 			}
 			
 			
